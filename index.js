9de6136143d46229195542e75ea83851d8079e50
Maciek <maciek@artdruk.eu>
2024-02-20 13:02:40 +0100

    index

--- index.js
+++ index.js
@@ -54,15 +54,15 @@ app.use(cors(
 
 
 // server
-app.listen(port, function(){
-    console.log('Waiting... http://localhost:'+ port);
-});
+// app.listen(port, function(){
+//     console.log('Waiting... http://localhost:'+ port);
+// });
 
 
 // key.pem i cert.pem to klucz prywatny i certyfika wygenerowany dla apacha z Lets Encrypt
-// const sslServer = https.createServer({
-// key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-// cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
-// },app)
+const sslServer = https.createServer({
+key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
+},app)
 
-// sslServer.listen(3443, ()=> console.log('Secure server on port 3443'))
+sslServer.listen(3443, ()=> console.log('Secure server on port 3443'))
