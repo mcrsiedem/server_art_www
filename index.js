6dc519b00ee751945dcaabc1f8771d299ade5f67
Maciek <maciek@artdruk.eu>
2024-03-25 15:36:59 +0100

    ssl

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const {port} = require('./config');
 
 
 // local
- const {server} = require('./actions/serverLocal');
- server.listen(port,()=>{  console.log("SERVER IS RUNNING")})
+//  const {server} = require('./actions/serverLocal');
+//  server.listen(port,()=>{  console.log("SERVER IS RUNNING")})
 
 
 // SSL na www
-// const {serverSSL} = require('./actions/serverSSL');
-// serverSSL.listen(3443, ()=> console.log('Secure server on port 3443_')) 
+const {serverSSL} = require('./actions/serverSSL');
+serverSSL.listen(3443, ()=> console.log('Secure server on port 3443_')) 
