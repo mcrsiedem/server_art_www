d4d86aceabbda6de36d6d944891b5219f27c611a
Maciek <maciek@artdruk.eu>
2024-05-09 16:07:24 +0200

    koszty

--- actions/connections.js
+++ actions/connections.js
@@ -88,6 +88,13 @@ class Connections {
         connection.query(sql, function (err, doc) {
         if (err) throw err;
         dane.push(doc)
+
+        } );
+
+        var sql = "select * from artdruk.koszty_dodatkowe where zamowienie_prime_id = '" + zamowienie_prime_id + "' ORDER BY id ASC";
+        connection.query(sql, function (err, doc) {
+        if (err) throw err;
+        dane.push(doc)
         res.status(200).json(dane);
         } );
 
