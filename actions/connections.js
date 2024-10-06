db7e18612739bc1ddf5f25e5f5ff2b75d011a878
Maciek <siedem@onet.pl>
2024-10-06 18:56:44 +0200

    procesy

--- actions/connections.js
+++ actions/connections.js
@@ -740,6 +740,8 @@ postTechnologieRest(req,res){
     let arkuszeEdit = req.body[6]
     let grupaWykonanEdit = req.body[7]
     let wykonaniaEdit = req.body[8]
+    let procesyElementowTech = req.body[9]
+    
 
 
     for (let produkty of produktyTechEdit) {
@@ -809,6 +811,35 @@ postTechnologieRest(req,res){
           fragment.wersja +        "','" +
           fragment.naklad +        "','" +
           fragment.info +        "'); ";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+        });
+      }
+
+      for (let procesElementu of procesyElementowTech) {
+        var sql =
+          "INSERT INTO artdruk.technologie_procesy_elementow (id,indeks,technologia_id,zamowienie_id,produkt_id,element_id,front_ilosc,front_kolor,back_ilosc,back_kolor,predkosc,narzad,mnoznik,nazwa,nazwa_id,rodzaj,typ,obszar,wykonczenie,procesor_domyslny) " +
+          "values ('" +
+          procesElementu.id +  "','" +
+          procesElementu.indeks +        "','" +
+          procesElementu.technologia_id +        "','" +
+          procesElementu.zamowienie_id +        "','" +
+          procesElementu.produkt_id +        "','" +
+          procesElementu.element_id +        "','" +
+          procesElementu.front_ilosc +        "','" +
+          procesElementu.front_kolor +        "','" +
+          procesElementu.back_ilosc +        "','" +
+          procesElementu.back_kolor+        "','" +
+          procesElementu.predkosc +        "','" +
+          procesElementu.narzad +        "','" +
+          procesElementu.mnoznik +        "','" +
+          procesElementu.nazwa +        "','" +
+          procesElementu.nazwa_id +        "','" +
+          procesElementu.rodzaj +        "','" +
+          procesElementu.typ +        "','" +
+          procesElementu.obszar +        "','" +
+          procesElementu.wykonczenie +        "','" +
+          procesElementu.procesor_domyslny +        "'); ";
         connection.query(sql, function (err, result) {
           if (err) throw err;
         });
