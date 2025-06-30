7b51263d4a8affe12939506823c0eb5112e1d769
Maciek <maciek@artdruk.eu>
2025-06-30 17:32:02 +0200

    zapis

--- actions/connections.js
+++ actions/connections.js
@@ -393,9 +393,18 @@ class Connections {
                  connection.query(sql, function (err, doc) {
                  if (err) throw err;
                  dane.push(doc)
+                 } );
+    
+
+     var sql = "select * from artdruk.view_technologie_grupy_wykonan_oprawa where status <4 and typ_grupy < 3 ORDER BY poczatek";
+                 connection.query(sql, function (err, doc) {
+                 if (err) throw err;
+                 dane.push(doc)
                  res.status(200).json(dane);
                  } );
     
+
+
          
              }
 
