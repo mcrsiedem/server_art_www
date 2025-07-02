a2e94eb950b2522d14c2f7c19e50628919be9fae
Maciek <siedem@onet.pl>
2025-07-02 21:03:19 +0200

    zapis

--- actions/connections.js
+++ actions/connections.js
@@ -683,7 +683,7 @@ class Connections {
              dane.push(doc)
              } );
 
-                      var sql = "select   DATE_FORMAT(min(data_spedycji), '%Y-%m-%d') AS `data_spedycji_min`, DATE_FORMAT(max(data_spedycji), '%Y-%m-%d') AS `data_spedycji_max`,  DateDiff((select max(data_spedycji) FROM  artdruk.view_zamowienia_kalendarz),(select min(data_spedycji) FROM  artdruk.view_zamowienia_kalendarz)) AS ilosc_dni  from artdruk.view_zamowienia_kalendarz ";
+                      var sql = "select   DATE_FORMAT(min(data_spedycji), '%Y-%m-%d') AS `data_spedycji_min`, DATE_FORMAT(max(data_spedycji), '%Y-%m-%d') AS `data_spedycji_max`,  DateDiff((select max(data_spedycji) FROM  artdruk.view_zamowienia_kalendarz),(select min(data_spedycji) FROM  artdruk.view_zamowienia_kalendarz)) AS ilosc_dni  from artdruk.view_zamowienia_kalendarz  ";
              connection.query(sql, function (err, doc) {
                 if (err){ throw err } 
              dane.push(doc)
