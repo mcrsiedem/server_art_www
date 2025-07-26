160e93310e51d2ab7b317860ae9c57d465492999
Maciek <siedem@onet.pl>
2025-07-26 13:21:43 +0200

    procesy

--- actions/zamowienia/ZamowienieUpdate.js
+++ actions/zamowienia/ZamowienieUpdate.js
@@ -120,37 +120,42 @@ for(let element of elementy.filter(x => x.update == true && x.insert != true) ){
 //-------------- fragmenty
 
         for(let row of fragmenty.filter(x => x.update == true && x.insert != true) ){
-          var sql =   "update  artdruk.zamowienia_fragmenty set  id = " + row.id+ ", zamowienie_id = " + row.zamowienie_id+ ", produkt_id = " + row.produkt_id+ ", element_id = " + row.element_id+ ", oprawa_id = " + row.oprawa_id+ ", naklad = " + row.naklad+ ", ilosc_stron = " + row.ilosc_stron+ ", wersja = '" + row.wersja+ "', info = '" + row.info+ "', typ = '" + row.typ+ "',  indeks = " + row.indeks+ " where global_id = " + row.global_id + ""
-          connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+          let dane =[row.id,row.zamowienie_id,row.produkt_id,row.element_id,row.oprawa_id,row.naklad,row.ilosc_stron,row.wersja,row.info,row.typ,row.indeks,row.global_id]
+          var sql =   "update  artdruk.zamowienia_fragmenty set  id =?, zamowienie_id =?, produkt_id =?, element_id =?, oprawa_id =?, naklad =?, ilosc_stron =?, wersja =?, info =?, typ =?,  indeks =? where global_id =?"
+          connection.execute(sql, dane, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
           }
         
           for(let row of fragmenty.filter(x => x.insert == true && x.delete != true) ){
-            var sql =   "INSERT INTO artdruk.zamowienia_fragmenty (id,zamowienie_id,produkt_id,element_id,oprawa_id,naklad,ilosc_stron,wersja,info,typ,indeks) "+
-            "values (" + row.id + "," + row.zamowienie_id + "," + row.produkt_id + "," + row.element_id + "," + row.oprawa_id + ",'" + row.naklad + "','" + row.ilosc_stron + "','" + row.wersja + "','" + row.info + "','" + row.typ + "'," + row.indeks + "); ";
-            connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+            let dane=[row.id,row.zamowienie_id,row.produkt_id,row.element_id,row.oprawa_id,row.naklad,row.ilosc_stron,row.wersja,row.info,row.typ,row.indeks]
+            var sql =   "INSERT INTO artdruk.zamowienia_fragmenty (id,zamowienie_id,produkt_id,element_id,oprawa_id,naklad,ilosc_stron,wersja,info,typ,indeks) values (?,?,?,?,?,?,?,?,?,?,?); ";
+            connection.execute(sql, dane,function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
             }
         
             for(let row of fragmenty.filter(x => x.delete == true && x.insert != true) ){
-                var sql =   "DELETE from artdruk.zamowienia_fragmenty where global_id=" + row.global_id;
-                connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+              let dane =[row.global_id]
+                var sql =   "DELETE from artdruk.zamowienia_fragmenty where global_id=?";
+                connection.execute(sql, dane,function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
                 }
 
   //-------------- procesy elementów
   for(let row of procesyElementow.filter(x => x.update == true && x.insert != true) ){
-    var sql =   "update  artdruk.zamowienia_procesy_elementow set  id = " + row.id+ ", zamowienie_id = " + row.zamowienie_id+ ",ilosc_uzytkow = " + row.ilosc_uzytkow+ ", produkt_id = " + row.produkt_id+ ", element_id = " + row.element_id+ ", proces_id = " + row.proces_id+ ", front_ilosc = '" + row.front_ilosc+ "', back_ilosc = '" + row.back_ilosc+ "', front_kolor = '" + row.front_kolor+ "', back_kolor = '" + row.back_kolor+ "', info = '" + row.info+ "', nazwa_id = " + row.nazwa_id+ ",  indeks = " + row.indeks+ " where global_id = " + row.global_id + ""
-    connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+    let dane=[row.id,row.zamowienie_id,row.ilosc_uzytkow, row.produkt_id,row.element_id,row.proces_id,row.front_ilosc,row.back_ilosc,row.front_kolor,row.back_kolor,row.info,row.nazwa_id,row.indeks,row.global_id]
+    var sql =   "update  artdruk.zamowienia_procesy_elementow set  id =?, zamowienie_id =?,ilosc_uzytkow =?, produkt_id =?, element_id =?, proces_id =?, front_ilosc =?, back_ilosc =?, front_kolor =?, back_kolor =?, info =?, nazwa_id =?,  indeks =? where global_id =?"
+    connection.execute(sql, dane, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
     }
   
   
     for(let row of procesyElementow.filter(x => x.insert == true && x.delete != true) ){
-      var sql =   "INSERT INTO artdruk.zamowienia_procesy_elementow (id,zamowienie_id,produkt_id,element_id,proces_id,ilosc_uzytkow,front_ilosc,back_ilosc,front_kolor,back_kolor,info,nazwa_id,indeks) "+
-      "values (" + row.id + "," + row.zamowienie_id + "," + row.produkt_id + "," + row.element_id + "," + row.proces_id + ",'" + row.ilosc_uzytkow + "','" + row.front_ilosc + "','" + row.back_ilosc + "','" + row.front_kolor + "','" + row.back_kolor + "','" + row.info + "'," + row.nazwa_id + "," + row.indeks + "); ";
-      connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+      let dane=[row.id,row.zamowienie_id,row.produkt_id,row.element_id,row.proces_id,row.ilosc_uzytkow,row.front_ilosc,row.back_ilosc,row.front_kolor,row.back_kolor,row.info,row.nazwa_id,row.indeks]
+
+      var sql =   "INSERT INTO artdruk.zamowienia_procesy_elementow (id,zamowienie_id,produkt_id,element_id,proces_id,ilosc_uzytkow,front_ilosc,back_ilosc,front_kolor,back_kolor,info,nazwa_id,indeks) values (?,?,?,?,?,?,?,?,?,?,?,?,?); ";
+      connection.execute(sql, dane,function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
       }
   
       for(let row of procesyElementow.filter(x => x.delete == true && x.insert != true) ){
-          var sql =   "DELETE from artdruk.zamowienia_procesy_elementow where global_id=" + row.global_id;
-          connection.query(sql, function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
+        let dane=[row.global_id]
+          var sql =   "DELETE from artdruk.zamowienia_procesy_elementow where global_id=?";
+          connection.execute(sql,dane,function (err, result) {       if (err){connection.query("rollback ", function (err, result) {   });   if (err) throw err;       }});
           }
 
      //-------------- oprawa
