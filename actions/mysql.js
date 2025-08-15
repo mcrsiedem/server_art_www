b3e5714f485bf2b7752ec84c7237b802e8c88f16
Maciek <siedem@onet.pl>
2025-08-15 16:43:33 +0200

    zapis

--- actions/mysql.js
+++ actions/mysql.js
@@ -2,5 +2,4 @@
 const mysql = require('mysql2');
 const {database} = require('../config');
 const connection = mysql.createConnection(database);
-// const pool = mysql.createPool(database);
 module.exports = connection;
