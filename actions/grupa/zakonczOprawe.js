4c153793c05b8b6f6c52b2083c5818017583b151
Maciek <siedem@onet.pl>
2025-09-03 22:50:33 +0200

    realizacja oprawy

--- actions/grupa/zakonczOprawe.js
+++ actions/grupa/zakonczOprawe.js
@@ -77,10 +77,10 @@ let Status = () =>{
 let OdwiezGrupe = () =>{ 
     return  new Promise((resolve,reject)=>{
   let data=[global_id]
-      var sql =   "SELECT status from artdruk.view_technologie_grupy_wykonan_oprawa where global_id=? ";
+      var sql =   "SELECT status,zrealizowano from artdruk.view_technologie_grupy_wykonan_oprawa where global_id=? ";
       connection.execute(sql, data,function (err, result) {     
             if (err) reject(err); 
-           resolve(result[0].status)
+           resolve({status:result[0].status, zrealizowano:result[0].zrealizowano})
         })
 })
 }
@@ -93,11 +93,11 @@ let SUMA_REALIZACJI = await  SprawdzIleBrakuje();  // wstaw wykonanie
 let BRAKUJACY_NAKLAD = await  Insert(SUMA_REALIZACJI);  // wstaw wykonanie
 let res2 = await  Historia(); // dodaj do historii
 let res3 = await  Status();  // zmieñ status grupy - w trakcie lub zakoñczone
-let nowy_status = await  OdwiezGrupe();  // sprawdza nowy status grupy
+let res4 = await  OdwiezGrupe();  // sprawdza nowy status grupy
 
 
 // pobierz tylko nowy status i odeślij go aby zaaktualizować
-res.status(200).json({status:"OK",insertId : id || 0,status_grupy:nowy_status, brakujacy_naklad:BRAKUJACY_NAKLAD });
+res.status(200).json({status:"OK",insertId : id || 0,status_grupy:res4.status, brakujacy_naklad:BRAKUJACY_NAKLAD,zrealizowano:res4.zrealizowano });
     } catch (error) {
         // Ten blok przechwyci błąd `err` przekazany przez `reject(err)`
         // z dowolnej z funkcji (Insert, Historia).
