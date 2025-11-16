ceaa81ccedc5b59649c23abb6b1c9ea7dc06547f
Maciek <siedem@onet.pl>
2025-11-16 17:27:55 +0100

    zapis

--- actions/zestawienia/getZestawienieUser.js
+++ actions/zestawienia/getZestawienieUser.js
@@ -1,21 +1,26 @@
 const { DecodeToken } = require('../logowanie/DecodeToken');
-
+const { pool } = require("../mysql");
 
 // ładuje ponownie uprawnienia z bazy do serwera
 
-     const getZestawienieUser = (req,res) =>{
+     const getZestawienieUser = async (req,res) =>{
         const OD_KIEDY = req.params['od']
         const DO_KIEDY = req.params['do']
         const KTO = req.params['kto'] 
 
-   console.log("OD_KIEDY:"+ OD_KIEDY)
-   console.log("DO_KIEDY:"+ DO_KIEDY)
-   console.log("KTO:"+ KTO)
-            //do testowania
-            // const token = req.params['token']
-            // console.log("insert token:"+ DecodeToken(token).id)
+  //  console.log("OD_KIEDY:"+ OD_KIEDY)
+  //  console.log("DO_KIEDY:"+ DO_KIEDY)
+  //  console.log("KTO:"+ KTO)
 
-            res.status(200).json("OK");
+    sql = "SELECT * FROM artdruk.view_realizacje_zestawienie where dodal_id = ? and utworzono > ? and utworzono < ? order by utworzono" ;
+              try {
+    const [rows] = await pool.execute(sql,[KTO,OD_KIEDY,DO_KIEDY]) 
+    results = [rows];
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error("Błąd w Kontrolerze:", err);
+    res.status(500).json({ error: "Błąd serwera." });
+  }   
      
          }
 
