59aea17e8c2c0e60338cd74c7dbee9d672069ca6
Maciek <maciek@artdruk.eu>
2026-02-06 11:40:13 +0100

    zapis

--- actions/zestawienia/getZestawienieUser.js
+++ actions/zestawienia/getZestawienieUser.js
@@ -13,10 +13,17 @@ const { pool } = require("../mysql");
   //  console.log("KTO:"+ KTO)
 
     sql = "SELECT * FROM artdruk.view_realizacje_zestawienie where dodal_id = ? and utworzono > ? and utworzono < (? + interval 1 day) and typ = 1 order by utworzono" ;
-              try {
-    const [rows] = await pool.execute(sql,[KTO,OD_KIEDY,DO_KIEDY]) 
-    results = [rows];
-    res.status(200).json(rows);
+    sql2 = "SELECT * FROM artdruk.view_realizacje_zestawienie_oprawa where dodal_id = ? and utworzono > ? and utworzono < (? + interval 1 day)  order by utworzono" ;
+             
+    try {
+    const [rows] = await pool.execute(sql,[KTO,OD_KIEDY,DO_KIEDY])    
+    const [rows2] = await pool.execute(sql2,[KTO,OD_KIEDY,DO_KIEDY]) 
+
+
+    let result2 = rows.concat(rows2);
+
+
+    res.status(200).json(result2);
   } catch (err) {
     console.error("Błąd w Kontrolerze:", err);
     res.status(500).json({ error: "Błąd serwera." });
