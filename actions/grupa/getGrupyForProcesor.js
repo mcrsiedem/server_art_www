539924e25fb300bec7e512f713695e0285ab9d2b
Maciek <siedem@onet.pl>
2026-03-15 19:51:23 +0100

    zapis

--- actions/grupa/getGrupyForProcesor.js
+++ actions/grupa/getGrupyForProcesor.js
@@ -7,9 +7,14 @@ const getGrupyForProcesor = async (req, res) => {
   var sql =
     "select * from artdruk.view_technologie_grupy_wykonan where poczatek >  (select min(poczatek) - interval 1 day from artdruk.view_technologie_grupy_wykonan where status <4 and procesor_id = ?)  and procesor_id = ? ORDER BY poczatek";
 
+
+      var sql2 =
+    "SELECT * FROM artdruk.view_technologie_wykonania where procesor_id = ? and status <4";
+
   try {
     const [rows] = await pool.execute(sql, [procesor_id, procesor_id]);
-    res.status(200).json([rows]);
+    const [wykonania] = await pool.execute(sql2, [procesor_id]);
+    res.status(200).json([rows,wykonania]);
   } catch (err) {
     console.error("Błąd w Kontrolerze:", err);
     res.status(500).json({ error: "Błąd serwera." });
