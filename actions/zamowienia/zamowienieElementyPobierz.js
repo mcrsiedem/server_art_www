b3469a4b7d96bddbfe406a1e2ab4a2a4c443657e
Maciek <maciek@artdruk.eu>
2026-03-31 11:13:00 +0200

    zapis

--- actions/zamowienia/zamowienieElementyPobierz.js
+++ actions/zamowienia/zamowienieElementyPobierz.js
@@ -6,12 +6,13 @@ const  zamowienieElementyPobierz = async (req,res)=>{
         const rok = req.params['rok']
 
 try {
-        const [elementy
+        const [elementy, nazwa
         ] = await Promise.all([
             pool.execute("select * from artdruk.view_technologie_elementy where nr = ? and rok=? ORDER BY id ASC", [nr,rok]),
+            pool.execute("select firma_nazwa, tytul from artdruk.view_zamowienia where nr = ? and rok=? ORDER BY id ASC", [nr,rok]),
         ]).then(results => results.map(r => r[0]));
 
-        const dane =[elementy]
+        const dane =[elementy,nazwa]
         res.status(200).json(dane);
 
     } catch (err) {
@@ -26,3 +27,12 @@ module.exports = {
     
 }
    
+
+    //  const sql1 = "SELECT * FROM artdruk.view_technologie_grupy_wykonan WHERE status < 4 AND typ_grupy < 3 ORDER BY poczatek";
+    //     const sql2 = "SELECT * FROM artdruk.view_technologie_grupy_wykonan_oprawa WHERE status < 4 AND typ_grupy < 3 ORDER BY poczatek";
+
+    //     // Odpalamy oba zapytania RÓWNOLEGLE (szybciej niż jedno po drugim)
+    //     const [result1, result2] = await Promise.all([
+    //         conn.query(sql1),
+    //         conn.query(sql2)
+    //     ]);
