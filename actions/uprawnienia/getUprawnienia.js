79154ba39d8609af1329869cadb8ed9f66160fd4
Maciek <siedem@onet.pl>
2026-04-04 21:53:45 +0200

    zapis

--- actions/uprawnienia/getUprawnienia.js
+++ actions/uprawnienia/getUprawnienia.js
@@ -1,26 +1,13 @@
-const { DecodeToken } = require('../logowanie/DecodeToken');
-const dataStore = require('./dataStore');
+const dataStore = require("./dataStore");
 
 // ładuje ponownie uprawnienia z bazy do serwera
 
-     const uprawnienia = (req,res) =>{
-
-
-            console.log("Uprawnienia załadowane!")
-            dataStore.loadPrivileges()
-
-            // do testowania
-            // const token = req.params['token']
-            // console.log("insert token:"+ DecodeToken(token).id)
-
-            res.status(200).json("OK");
-     
-         }
-
+const uprawnienia = (req, res) => {
+  console.log("Uprawnienia załadowane!");
+  dataStore.loadPrivileges();
+  res.status(200).json("OK");
+};
 
 module.exports = {
-  uprawnienia
-    
-}
- 
-
+  uprawnienia,
+};
