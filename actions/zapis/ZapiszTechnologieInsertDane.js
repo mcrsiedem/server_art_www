246650c57c15248be4c1b3336b9b0c7208f6c258
Maciek <maciek@artdruk.eu>
2026-05-07 12:09:05 +0200

    zapis

--- actions/zapis/ZapiszTechnologieInsertDane.js
+++ actions/zapis/ZapiszTechnologieInsertDane.js
@@ -4,7 +4,7 @@ const zapiszTechnologieInsertDane = (req,res) =>{
   let promises = [];
   let daneTech = req.body[0]
 
-
+console.log("Nowy zapis to tu")
 
   var sql =   "INSERT INTO artdruk.technologie (nr,rok,tytul,firma_id,klient_id,zamowienie_id,autor_id,uwagi,opiekun_id,data_przyjecia,data_spedycji,data_materialow,stan,status,etap) "+
   "values ('" + daneTech.nr + "','" + daneTech.rok + "','" + daneTech.tytul + "','" + daneTech.firma_id + "','" + daneTech.klient_id + "','" + daneTech.zamowienie_id + "','" + daneTech.autor_id + "','" + daneTech.uwagi + "','" + daneTech.opiekun_id + "'," +ifNoDateSetNull( daneTech.data_przyjecia) + "," +ifNoDateSetNull( daneTech.data_spedycji) + "," + ifNoDateSetNull(daneTech.data_materialow) + "," + daneTech.stan + "," + daneTech.status + "," + daneTech.etap + "); ";
